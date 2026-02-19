f4a22426978597872356a3eba36975479d9a2941
ayame1334 <ayame1334@gmail.com>
2026-02-19 17:19:47 +0800

    Quartz sync: Feb 19, 2026, 5:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "ATAME",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,19 +23,19 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "EB Garamond",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#f4ecd8", // 匹配你星系的底色
+          secondary: "#284b63", // 链接颜色
+          tertiary: "#84a59d", // 强调色
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
